06b38dd2745620cc6e1df9784c6acbfeee811dd0
Roman Kordasiewicz <kordasiewicz@google.com>
2025-12-03 16:13:50 -0500

    msg

--- routes/index.tsx
+++ routes/index.tsx
@@ -23,7 +23,7 @@ export default function Home(props: PageProps<HomeProps>) {
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <h1 class="my-2">
-          Click count - Deploy
+          Click count - Deploy TEST
         </h1>
         <img
           src="mouse-cursor.svg"
